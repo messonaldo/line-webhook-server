87f21f6687b86227ddabbe26d9b6b5d995d5fafa
messonaldo <109655595+messonaldo@users.noreply.github.com>
2025-05-26 21:27:57 +0800

    main.py

--- main.py
+++ main.py
@@ -4,13 +4,10 @@ app = Flask(__name__)
 
 @app.route("/webhook", methods=["POST"])
 def webhook():
-    # 這行強制解析 JSON
-    data = request.get_json(force=True)
-
-    print("===== Webhook Triggered =====")
-    print(data)  # ✅ 印出完整 JSON 結構
+    raw_body = request.get_data(as_text=True)
+    print("===== 原始 Webhook 內容 =====")
+    print(raw_body)
     print("=============================")
-
     return "OK"
 
 app.run(host="0.0.0.0", port=5000)
